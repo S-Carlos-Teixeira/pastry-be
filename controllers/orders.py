767eeeffba5811fb36f29e39234665fd676b11d3
Carlos Teixeira <s.carlosateixeira@gmail.com>
2023-02-19 00:46:35 +0000

    working on orders controller

--- controllers/orders.py
+++ controllers/orders.py
@@ -33,18 +33,26 @@ def create_order():
             user_id=g.current_user.id, is_active=True
         ).first()
         current_user = g.current_user
+        
+        if not cart:
+            return {"message": "Cart not found"}, HTTPStatus.NOT_FOUND
+        if not current_user:
+            return {"message": "Unauthorized"}, HTTPStatus.UNAUTHORIZED
         total = 0
-        for product in cart.products:
-            product_total = product.product.price * product.quantity
+        for cart_item in cart.products:
+            product_total = cart_item.product.price * cart_item.quantity
             total += product_total
-
         order_dictionary = {
             "cart_id": cart.id,
             "user_id": current_user.id,
             "total": total,
         }
-        order = order_schema.load(order_dictionary)
-        order.save()
+        order = OrderModel.query.filter_by(user_id=g.current_user.id, is_active=True
+        ).first()
+        print(order)
+        if not order:
+            order = order_schema.load(order_dictionary)
+            order.save()
         return order_schema.jsonify(order)
     except ValidationError as e:
         return {"errors": e.messages, "message": "Something went wrong"}

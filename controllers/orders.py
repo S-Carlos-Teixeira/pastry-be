58d8e8745ceac165290f1b1f23929c5defcfd921
Carlos Teixeira <s.carlosateixeira@gmail.com>
2023-02-19 01:28:38 +0000

    working on orders controller

--- controllers/orders.py
+++ controllers/orders.py
@@ -56,3 +56,36 @@ def create_order():
         return order_schema.jsonify(order)
     except ValidationError as e:
         return {"errors": e.messages, "message": "Something went wrong"}
+
+@router.route("/order", methods=["PUT", "PATCH"])
+@secure_route
+def update_order():
+    try:
+        order_dictionary = request.get_json()
+        order = OrderModel.query.filter_by(user_id=g.current_user.id, is_active=True).first()
+        if not order:
+            return {"message": "Order not found"}, HTTPStatus.NOT_FOUND
+        
+        cart = CartModel.query.get(order.cart_id)
+        if not cart:
+            return {"message": "Cart not found"}, HTTPStatus.NOT_FOUND
+        
+        order = order_schema.load(order_dictionary, instance=order, partial=True)
+        order.save()
+        if order.status == "paid":
+            cart.is_active = False
+            cart.save()
+        if order.status == "cancelled":
+            order.is_active = False
+            cart.is_active = False
+            order.save()
+            cart.save()
+        if order.status == "delivered":
+            order.is_active = False
+            cart.is_active = False
+            order.save()
+            cart.save()
+
+        return order_schema.jsonify(order)
+    except ValidationError as e:
+        return {"errors": e.messages, "message": "Something went wrong"}
